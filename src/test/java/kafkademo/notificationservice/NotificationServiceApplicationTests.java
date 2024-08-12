129c3ba73da54a43e2a758615f879eecfb933342
reamfox <sashafarion@gmail.com>
2024-08-12 12:39:53 +0300

    innit commit

---
+++ src/test/java/kafkademo/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package kafkademo.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
