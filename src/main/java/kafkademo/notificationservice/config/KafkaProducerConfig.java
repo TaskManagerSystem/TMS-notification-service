af81c3e3fd6226fc4e6a23aff76a9f004a8998b3
Oleksandr Farion <sashafarion@gmail.com>
2024-08-23 11:30:55 +0300

    refactor data for sending (#6)

--- src/main/java/kafkademo/notificationservice/config/KafkaProducerConfig.java
+++ src/main/java/kafkademo/notificationservice/config/KafkaProducerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -17,16 +18,16 @@ public class KafkaProducerConfig {
     private String bootstrapServer;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
--- src/main/java/kafkademo/notificationservice/kafka/KafkaProducer.java
+++ src/main/java/kafkademo/notificationservice/kafka/KafkaProducer.java
@@ -1,5 +1,7 @@
 package kafkademo.notificationservice.kafka;
 
+import com.example.dto.VerificationData;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -10,14 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer {
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendEmailToValidate(String email, Long chatId, String token) {
         try {
-            String message = token + ":" + email + ":" + chatId;
+            VerificationData verificationData = new VerificationData();
+            verificationData.setEmail(email);
+            verificationData.setChatId(chatId.toString());
+            verificationData.setCreatedAt(LocalDateTime.now());
+            verificationData.setPresent(false);
             String topic = "email-validation-topic";
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<>(topic, message);
+            ProducerRecord<String, Object> record =
+                    new ProducerRecord<>(topic, token, verificationData);
             kafkaTemplate.send(record);
             log.info("Sent record: {} to the topic {}", record, topic);
         } catch (Exception e) {
